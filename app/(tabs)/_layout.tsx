603f0f8a36a402ddd5b2bb31efd8b3c865e500f7
gulsemins <gulsemin111@gmail.com>
2024-08-30 12:54:39 +0300

    changed the stack name according to subject id

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,48 +3,50 @@ import Foundation from "@expo/vector-icons/Foundation";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-import { Tabs } from "expo-router";
+import { Stack, Tabs } from "expo-router";
 import React from "react";
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#e1575d",
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#e1575d",
 
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Foundation name="home" size={28} color={color} />
-          ),
-          tabBarLabel: "",
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="chat"
-        options={{
-          title: "Chat",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="wechat" size={28} color={color} />
-          ),
-          tabBarLabel: "",
-        }}
-      />
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Foundation name="home" size={28} color={color} />
+            ),
+            tabBarLabel: "",
+          }}
+        />
+        <Tabs.Screen
+          name="chat"
+          options={{
+            title: "Chat",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="wechat" size={28} color={color} />
+            ),
+            tabBarLabel: "",
+          }}
+        />
 
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="person" size={28} color={color} />
-          ),
-          tabBarLabel: "",
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="person" size={28} color={color} />
+            ),
+            tabBarLabel: "",
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
--- app/setDetails/_layout.tsx
+++ app/setDetails/_layout.tsx
@@ -3,54 +3,74 @@ import Foundation from "@expo/vector-icons/Foundation";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-import { Tabs } from "expo-router";
+import { Stack, Tabs, useLocalSearchParams } from "expo-router";
 import React from "react";
 
 export default function TabLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "#e1575d",
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="[id]"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="wechat" size={28} color={color} />
-          ),
-        }}
-      />
+  // TODO: search how to reach from [id] to root e_layout file to change tha stack name
 
-      <Tabs.Screen
-        name="flashcards"
-        options={{
-          title: "flashcards",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="wechat" size={28} color={color} />
-          ),
+  const { id } = useLocalSearchParams();
+  const sets = [
+    {
+      idSet: "1",
+      name: "Mathematics",
+      desc: "Explore the world of numbers, equations, and geometric shapes.",
+    },
+    {
+      idSet: "2",
+      name: "Physics",
+      desc: "Understand the fundamental principles of the universe, including mechanics.",
+    },
+  ];
+  const found = sets.find((set) => set.idSet == id);
+  return (
+    <>
+      <Stack.Screen options={{ title: found?.name }} />
+
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#e1575d",
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="match"
-        options={{
-          title: "match",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="quiz" size={28} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="tests"
-        options={{
-          title: "tests",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="quiz" size={28} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="[id]"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="wechat" size={28} color={color} />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="flashcards"
+          options={{
+            title: "flashcards",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="wechat" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="match"
+          options={{
+            title: "match",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="quiz" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="tests"
+          options={{
+            title: "tests",
+            tabBarIcon: ({ color }) => (
+              <MaterialIcons name="quiz" size={28} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
